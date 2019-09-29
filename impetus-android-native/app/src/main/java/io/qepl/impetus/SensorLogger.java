41911b20e287034bd369d2e0b1020e731d6f969d
Jake Love <jake.love.mail@gmail.com>
2019-09-29 19:22:03 +0100

    Added class to handler sensor logging

---
+++ impetus-android-native/app/src/main/java/io/qepl/impetus/SensorLogger.java
@@ -0,0 +1,67 @@
+package io.qepl.impetus;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import java.util.ArrayList;
+
+public class SensorLogger implements SensorEventListener{
+    
+    private Boolean isLogging = false;
+
+    private ArrayList<SensorEvent> sensorLog;
+
+
+    SensorLogger(Context context) {
+
+        int sensorDelay = SensorManager.SENSOR_DELAY_FASTEST;
+        int[] sensorTypes = {Sensor.TYPE_LINEAR_ACCELERATION, Sensor.TYPE_ACCELEROMETER, Sensor.TYPE_ORIENTATION, Sensor.TYPE_GYROSCOPE};
+
+        android.hardware.SensorManager manager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+
+        for (int sensorType: sensorTypes) {
+            manager.registerListener(this, manager.getDefaultSensor(sensorType), sensorDelay);
+        }
+
+        reset();
+    }
+
+    ArrayList<SensorEvent> finishLogging() {
+
+        isLogging = false;
+
+        return sensorLog;
+
+    }
+
+    void startLogging() {
+
+        isLogging = true;
+
+    }
+
+
+    void reset() {
+
+        sensorLog = new ArrayList<>();
+
+    }
+
+
+    public void onSensorChanged(SensorEvent event) {
+
+        if(isLogging) {
+
+            sensorLog.add(event);
+
+        }
+
+    }
+
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+}
