19c383fa80517bd87e3fd294a3a08bf1f6517f85
Jake Love <jake.love.mail@gmail.com>
2019-09-29 19:23:04 +0100

    Hooked it all together

--- impetus-android-native/app/src/main/java/io/qepl/impetus/PromptQueue.java
+++ impetus-android-native/app/src/main/java/io/qepl/impetus/PromptQueue.java
@@ -11,44 +11,63 @@ import android.app.Activity;
 public class PromptQueue {
 
     private Integer[] valueQueue = {1, 2, 3, 4};
-    private Context context;
     private Random PRNG = new Random();
 
+    private Context context;
+
+
     PromptQueue(Context context) {
+
         this.context = context;
 
         for (int i = 0; i < 4 ; i++) {
+
             this.valueQueue[i] = randomDigit();
+
         }
 
         this.updatePrompt();
+
     }
 
+
     private void updatePrompt() {
+
         Log.e("HELLO", "updatePrompt: ");
+
+
         EditText prompt0 = ((Activity)context).findViewById(R.id.textPrompt0);
         prompt0.setText(String.format(Locale.ENGLISH, "%d",valueQueue[0]));
+
 
         EditText prompt1 = ((Activity)context).findViewById(R.id.textPrompt1);
         prompt1.setText(String.format(Locale.ENGLISH, "%d",valueQueue[1]));
 
+
         EditText prompt2 = ((Activity)context).findViewById(R.id.textPrompt2);
         prompt2.setText(String.format(Locale.ENGLISH, "%d",valueQueue[2]));
 
+
         EditText prompt3 = ((Activity)context).findViewById(R.id.textPrompt3);
         prompt3.setText(String.format(Locale.ENGLISH, "%d",valueQueue[3]));
+
     }
+
 
     public void next() {
 
         for (int i = 0; i < 3 ; i++) {
+
             this.valueQueue[i] = this.valueQueue[i + 1];
+
         }
 
         this.valueQueue[3] = randomDigit();
 
         this.updatePrompt();
+
     }
+
 
     private Integer randomDigit() {
         return PRNG.nextInt(9 + 1);
