80e97b3792a6b4703a53ad5a30dc6381399d785b
Jake Love <jake.love.mail@gmail.com>
2019-10-01 12:04:12 +0100

    Added shebangs to runnable scripts

--- src/impetus.py
+++ src/impetus.py
@@ -1,7 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import json
+
 import preprocessor
+import config
+
 
 def main():
-	preprocessor.process
 
+    data_set = []
+
+    for fn in os.listdir(config.SENSOR_FEED_DIR):
+
+        with open(os.path.join(config.SENSOR_FEED_DIR, fn)) as sensorevent:
+
+            preprocessor.process(json.load(sensorevent))
+
+    
 if __name__ == '__main__':
-	main()
+    main()
