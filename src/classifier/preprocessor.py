8db514a46905e93cc03373bed43914d486290549
Jake Love <jake.love.mail@gmail.com>
2019-10-03 11:23:25 +0100

    Preprocessor finalised

--- src/classifier/preprocessor.py
+++ src/classifier/preprocessor.py
@@ -1,75 +1,49 @@
+import operator
+
+import progressbar
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from scipy import interpolate
-from tsfresh import extract_features
-
-def processfeatures(dataset):
-
-	labels = [item['entry'] for item in dataset]
-
-	sensordata = extractsensordata(dataset)
-
-	df = makedataframe(sensordata)
-
-	print(df.head())
-
-def makedataframe(sensordata):
-
-	head = sensordata[0]
-
-	columncount  = len(head)
-	columnlength = len(head[0])
-	itemcount    = len(sensordata)
-
-	columnheaders = ['sensor' + str(n) for n in range(columncount)]
-
-	datamatrix = np.vstack([np.transpose([np.repeat(i, len(sensor[0]))] + sensor) for i, sensor in enumerate(sensordata)])
-
-	return pd.DataFrame(datamatrix)
 
 
-def extractsensordata(dataset):
-	return standardise(list(map(resample, dataset)))
+
+def process(dataset):
+
+	# filter out empty sensor logs
+	dataset = [s for s in dataset if len(s['sensor_log']['android.sensor.orientation']['values']) > 0]
+
+	bar = progressbar.ProgressBar(max_value=len(dataset), redirect_stdout=True, poll_interval=0.0001)
+	bar.update(0)
+
+	for n, item in enumerate(dataset):
+
+		item['processed_log'] = processlog(item['sensor_log'])
+		bar.update(n)
+
+	bar.finish()
+
+	return dataset
 
 
-def standardise(dataset):
+def processlog(sensorlog):
 
-	head = dataset[0]
+	processed = []
 
-	sumstds = np.zeros(len(head))
-
-	for sensorlog in dataset:
-		sumstds += np.array([np.std(s) for s in sensorlog])
-
-	meanstds = np.divide(sumstds, len(dataset))
-
-	return [[center(np.divide(s, meanstds[i])) for i, s in enumerate(sensorlog)] for sensorlog in dataset]
-
-
-def resample(item):
-
-	resampledlogs = []
-	log = item['sensor_log']
-
-	for sensorname, timeseries in log.items():
+	for sensorname, timeseries in sorted(sensorlog.items(), key=operator.itemgetter(0)):
 
 		transposed = [list(s) for s in zip(*timeseries['values'])]
 
 		domain_old = timeseries['domain']
-		domain_new = np.linspace(domain_old[0], domain_old[-1], 50)
+		domain_new = np.linspace(domain_old[0], domain_old[-1], 100)
 
 		for s in transposed:
 
-			f = interpolate.interp1d(domain_old, s, kind='cubic')
-			resampledlogs.append(f(domain_new))
+			f = interpolate.interp1d(domain_old, s, kind='linear')
+			s_ = f(domain_new)
+			
+			processed.append(np.subtract(s_, np.mean(s_)))
 
-	return resampledlogs
-
-
-def center(X):
-
-    return np.subtract(X, np.mean(X))
-
-
+	return processed 
